5b82941f25d7e26a6a3193089b3403f7620eb94e
yuniro <sergey.r1130@gmail.com>
2022-02-22 14:12:18 +0200

    first commit

---
+++ src/components/HelloWorldWrap.vue
@@ -0,0 +1,16 @@
+<template>
+  <HelloWorld />
+</template>
+
+<script>
+  import HelloWorld from './HelloWorld.vue';
+  import vuetify from '../plugins/vuetify';
+  export default {
+    name: 'HelloWorldWrap',
+      vuetify,
+    components: {HelloWorld}
+  }
+</script>
+<style>
+  @import 'https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.min.css';
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import wrap from '@vue/web-component-wrapper'
+import HelloWorldWrap from './components/HelloWorldWrap'
 
 Vue.config.productionTip = false
 
+const CustomElement = wrap(Vue, HelloWorldWrap);
+
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
+
+window.customElements.define('hapi-elements', CustomElement);
